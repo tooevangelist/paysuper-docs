20aa88dc8855f5d14a3e9bad9e956d93f0384987
Valentin Vesvalo <vesvalo@mail.ru>
2019-03-14 17:59:21 +0300

    test build with travis

--- config.rb
+++ config.rb
@@ -46,7 +46,7 @@ configure :build do
   # out the following two lines has been known to help
   activate :minify_css
   activate :minify_javascript
-  activate :relative_assets
+  #activate :relative_assets
   activate :asset_hash
   # activate :gzip
 end
